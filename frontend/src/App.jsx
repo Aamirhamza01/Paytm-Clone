c71694e4107ddf7abd8c624020954a1dae99c9b7
Aamir-hamza <anasturk14287@gamil.com>
2026-07-19 21:19:47 +0530

    frontend-Add

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,22 @@
+import{BrowserRouter,Route,Routes} from "react-router-dom";
 
-function App() {
+import{Signup} from "./pages/Signup";
+import{Signin} from "./pages/Signin";
+import{Dashboard} from "./pages/Dashboard";
+import{SendMoney} from "./pages/SendMoney";
 
-  return (
-    <div>
-        Hello world
-    </div>
+function App(){
+  return(
+    <>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/signup" element={<Signup />}/>
+      <Route path="/signin" element={<Signin />}/>
+      <Route path="/dashboard" element={<Dashboard />}/>
+      <Route path="/send" element={<SendMoney />}/>
+    </Routes>
+    </BrowserRouter>
+    </>
   )
 }
 
